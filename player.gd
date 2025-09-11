642dd29e0e55e85c5df4c796e78f3e8fd3b5948b
Mary Lynn <mlynn@ftstudent.org>
2025-09-11 12:25:08 -0400

    Shooter moving left
    Added code so when i press the left arrow my shooter will move left.

--- player.gd
+++ player.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 
 
-func _ready() -> void:
+func _ready():
 	pass 
 	
 	
 	
-func _process(delta: float) -> void:
-		pass
+func _process(delta):
+	if Input.is_action_pressed("player_right"):
+		position.x +=10
