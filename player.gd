6b3d161b168cd0a77d6d556bc7bc60ecd5c585c4
Mary Lynn <mlynn@ftstudent.org>
2025-09-15 11:10:15 -0400

    player left and right
    changed the code so that shooter can go left and right and stop when hitting camera border.

--- player.gd
+++ player.gd
@@ -8,8 +8,8 @@ func _ready():
 	
 	
 func _process(delta):
-	if Input.is_action_pressed("player_right"):
+	if Input.is_action_pressed("player_right") and position.x < 1110:
 		position.x +=10
-	if Input.is_action_pressed("player_left"):
+	if Input.is_action_pressed("player_left") and position.x > 45:
 		position.x -=10
 		
